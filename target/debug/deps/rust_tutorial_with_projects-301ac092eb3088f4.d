6e24764713510d104da916f4308d4ff3d04e14d0
Ferivonus <ferivonus@hotmail.com>
2023-07-17 04:05:52 +0300

    hello world rust

---
+++ target/debug/deps/rust_tutorial_with_projects-301ac092eb3088f4.d
@@ -0,0 +1,5 @@
+C:\Users\fahre\Desktop\yazılım\my codes\RUST\tutorial\rust-tutorial-with-projects\target\debug\deps\rust_tutorial_with_projects-301ac092eb3088f4.rmeta: src\main.rs
+
+C:\Users\fahre\Desktop\yazılım\my codes\RUST\tutorial\rust-tutorial-with-projects\target\debug\deps\rust_tutorial_with_projects-301ac092eb3088f4.d: src\main.rs
+
+src\main.rs:
