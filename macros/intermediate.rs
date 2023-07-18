6ae745ebd11c54af21a33d613a8d5bc0f513133d
Ferivonus <ferivonus@hotmail.com>
2023-07-18 16:28:36 +0300

    Macro's intermediate and introduction parts are added for first time.

--- macros/intermediate.rs
+++ macros/intermediate.rs
@@ -1,1 +1,43 @@
-// will be added:
+macro_rules! calculate {
+    // Define a pattern that matches when the macro is invoked with an addition operation
+    (addition $x:expr, $y:expr) => {
+        $x + $y
+    };
+    
+    // Define a pattern that matches when the macro is invoked with a subtraction operation
+    (subtraction $x:expr, $y:expr) => {
+        $x - $y
+    };
+    
+    // Define a pattern that matches when the macro is invoked with a multiplication operation
+    (multiplication $x:expr, $y:expr) => {
+        $x * $y
+    };
+    
+    // Define a pattern that matches when the macro is invoked with a division operation
+    (division $x:expr, $y:expr) => {
+        $x / $y
+    };
+}
+
+fn main() {
+    let result1 = calculate!(addition 5, 3); // Invoke the `calculate` macro for addition
+    println!("Result of addition: {}", result1);
+
+    let result2 = calculate!(subtraction 10, 7); // Invoke the `calculate` macro for subtraction
+    println!("Result of subtraction: {}", result2);
+
+    let result3 = calculate!(multiplication 4, 6); // Invoke the `calculate` macro for multiplication
+    println!("Result of multiplication: {}", result3);
+
+    let result4 = calculate!(division 15, 3); // Invoke the `calculate` macro for division
+    println!("Result of division: {}", result4);
+}
+/*
+    The output of this code:
+    Result of addition: 8
+    Result of subtraction: 3
+    Result of multiplication: 24
+    Result of division: 5
+
+*/
--- macros/introduction.rs
+++ macros/introduction.rs
@@ -1,1 +1,30 @@
-// will be added:
+// Define a simple custom macro called `greet`
+macro_rules! greet {
+    // Define a pattern that matches when the macro is invoked without any arguments
+    () => {
+        println!("Hello, world!");
+    };
+    
+    // Define a pattern that matches when the macro is invoked with a single argument
+    ($name:expr) => {
+        println!("Hello, {}!", $name);
+    };
+    
+    // Define a pattern that matches when the macro is invoked with multiple arguments
+    ($name:expr, $greeting:expr) => {
+        println!("{} {}", $greeting, $name);
+    };
+}
+
+fn main() {
+    greet!(); // Invoke the `greet` macro without any arguments
+    greet!("feri"); // Invoke the `greet` macro with a single argument
+    greet!("von", "Greetings :3"); // Invoke the `greet` macro with multiple arguments
+}
+/*
+
+    the output of this code:
+
+    You can easily understand that.
+
+*/
